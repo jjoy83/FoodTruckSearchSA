c32858c9906c4efcbfb5bf2d1c489c067fc01edc
joy <pjencyjoy@hotmail.com>
2022-01-15 15:59:14 -0500

    Fixed some more issues

--- src/FoodTruckSearchApp/FoodTruckSearchFrontEnd/wwwroot/js/site.js
+++ src/FoodTruckSearchApp/FoodTruckSearchFrontEnd/wwwroot/js/site.js
@@ -32,25 +32,33 @@ function RenderSearchResultsOnMap()
     debugger;
     var base64Image = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABkAAAAcBAMAAABmCgnjAAAAFVBMVEVHcEz///8aGhpsuy2s4oDRuPMaADiElDMGAAAAAXRSTlMAQObYZgAAAFNJREFUeF7FkLENgDAQA52GGiIxwD8bPBsgBmD/aXCs6JMNcpWtk1wYx8SO6gO2N3kWtDuJBc39UtYTPEc69BIpTVqLgjJOpS7tQ1I5NyiGia2rHyT7OTg7xhBoAAAAAElFTkSuQmCC';
     var map = new Microsoft.Maps.Map('#myMap');
-    var mapCenterLocation = map.getCenter();
-    var mapCenterPoint = map.tryLocationToPixel(mapCenterLocation);
-    var mapCenterPointPlus50px = new Microsoft.Maps.Point(mapCenterPoint.x + 50, mapCenterPoint.y);
-    var mapCenterLocationPlus50px = map.tryPixelToLocation(mapCenterPointPlus50px);
 
     var searchTextParam = document.getElementsByName('searchText')[0].value;
     var latitudeParam = document.getElementsByName('latitude')[0].value;
     var longitudeParam = document.getElementsByName('longitude')[0].value;
 
-    fetch('https://localhost:44307/FoodTruckSearch/SearchFoodTruck?searchText='+searchTextParam+'&latitude='+latitudeParam+'&longitude='+longitudeParam)
-        .then(response => response.json())
-        .then(response => {
-            for (i = 0; i < response.FoodTruckDataList.Length; i++) {
-                var mapPoint = new Microsoft.Maps.Point(response.FoodTruckDataList[i].x, response.FoodTruckDataList[i].y);
-                //Add a pushpin at the user's location.
-                var pin = new Microsoft.Maps.Pushpin(mapPoint, { icon: base64Image, anchor: new Microsoft.Maps.Point(12, 28) }, { text: 'Food truck', title: response.FoodTruckDataList[i].Applicant, subTitle: response.FoodTruckDataList[i].FoodItems});
-                map.entities.push(pin);
+    fetch('https://localhost:44307/FoodTruckSearch/SearchFoodTruck?searchText=' + searchTextParam + '&latitude=' + latitudeParam + '&longitude=' + longitudeParam)
+        .then(async (response) => {
 
+            // get json response here
+            let data = await response.json();
+            
+            if (response.status === 200) {
+                alert(data);
+                var result = Json.parse(data);
+                for (item in result.FoodTruckDataList) {
+                    var mapPoint = new Microsoft.Maps.Point(item.x, item.y);
+                    //Add a pushpin at the user's location.
+                    var pin = new Microsoft.Maps.Pushpin(mapPoint, { icon: base64Image, anchor: new Microsoft.Maps.Point(12, 28) }, { text: 'Food truck', title: item.Applicant, subTitle: item.FoodItems });
+                    map.entities.push(pin);
+
+                }
+            } else {
+                document.getElementsByName('searchText')[0].innerHTML = "Error occured while executing the search. Please try again later or contact support if problem persists."
             }
-        });
-    
+
+        })
+        .catch((err) => {
+            console.log(err);
+        })
 }
